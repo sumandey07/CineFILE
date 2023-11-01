bf2e786065205440c84394f83df11c03ba196dab
Suman <sumankumardey89@gmail.com>
2023-11-01 18:24:11 +0530

    update

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import "./style.scss";
-
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 
 const PageNotFound = () => {
-    return (
-        <div className="pageNotFound">
-            <ContentWrapper>
-                <span className="bigText">404</span>
-                <span className="smallText">Page not found!</span>
-            </ContentWrapper>
-        </div>
-    );
+  return (
+    <div className="pageNotFound">
+      <ContentWrapper className="contentWrapper">
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+        <Link className="button btn btn-outline-info mt-5 rounded-5" to="/">
+          Go Home<pre className="btnText">   &rarr;</pre>
+        </Link>
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default PageNotFound;
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,8 +1,4 @@
 import React, { useState, useEffect } from "react";
-import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
-import Img from "../../components/lazyLoadImage/Img";
-import cinefile from "../../assets/cinefile.png";
-import logo from "../../assets/logo.png";
 import "./style.scss";
 import Spinner from "../../components/spinner/Spinner";
 
@@ -26,39 +22,35 @@ const About = () => {
 
   return (
     <div className="about">
-      <ContentWrapper>
-        {loading && <Spinner initial={true} />}
-        {!loading && (
-          <div className="part">
-            <div className="first">
-              <Img className="logo" src={logo} />
-              <Img className="logoImg" src={cinefile} />
-            </div>
-            <div className="second">
-              <p className="para">About Us</p>
-            </div>
-            <div className="third">
-              <p className="paragraph">
-                This is a simple movie search website. It uses the TMDB API to
-                fetch the movies. It is built using ReactJS and Redux. It uses
-                React Hooks and React Router. It also uses Redux Thunk for async
-                API. I have used SCSS for styling. It is a responsive app. It
-                uses Lazy Loading for images. It also uses Intersection Observer
-                API for infinite scrolling. It uses React Helmet for SEO. It
-                uses React Toastify for toast messages. It uses React Spinners
-                for loading spinners. It uses React Icons for icons. It uses
-                Axios for API calls. It uses React Lazy Load Image Component for
-                lazy loading images. It uses React Content Loader for loading
-                skeleton. It uses React Infinite Scroll Component for infinite
-                scrolling. It uses React Router for routing. It uses React Redux
-                for state management. It uses Redux Thunk for async API. It uses
-                Redux Dev Tools for debugging. It uses Redux Logger for logging.
-                It uses Redux Persist for persisting state.
-              </p>
-            </div>
+      {loading && <Spinner initial={true} />}
+      {!loading && (
+        <article className="container">
+          <h1>About Us</h1>
+          <div>
+            <p>
+              This is a simple movie search website. It uses the TMDB API to
+              fetch the movies. It is built using ReactJS and Redux. It uses
+              React Hooks and React Router. It also uses Redux Thunk for async
+              API. I have used SCSS for styling. It is a responsive app.
+            </p>
+            <p>
+              It uses Lazy Loading for images. It also uses Intersection
+              Observer API for infinite scrolling. It uses React Helmet for SEO.
+              It uses React Toastify for toast messages. It uses React Spinners
+              for loading spinners. It uses React Icons for icons.
+            </p>
+            <p>
+              It uses Axios for API calls. It uses React Lazy Load Image
+              Component for lazy loading images. It uses React Content Loader
+              for loading skeleton. It uses React Infinite Scroll Component for
+              infinite scrolling. It uses React Router for routing. It uses
+              React Redux for state management. It uses Redux Thunk for async
+              API. It uses Redux Dev Tools for debugging. It uses Redux Logger
+              for logging. It uses Redux Persist for persisting state.
+            </p>
           </div>
-        )}
-      </ContentWrapper>
+        </article>
+      )}
     </div>
   );
 };
